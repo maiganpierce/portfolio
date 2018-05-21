7a1bdbafd853572643d39a9e0f6eed910ab0aedd
Maigan Pierce <maiganpierce@gmail.com>
2018-05-21 12:56:31 -0500

    placeholder image as project backgrounds until I can figure something else out that looks better

--- app/main.js
+++ app/main.js
@@ -9,19 +9,19 @@
 				title:"Twitch Status App",
 				description:"Uses JavaScript, HTML, CSS, AngularJS, Bootstrap",
 				url:"https://maiganpierce.github.io/Twitch-Status-App/",
-				background:"content/twitch-status-app.PNG"
+				background:"content/placeholder1.png"
 			},
 			{
 				title:"Weather App",
 				description:'Uses HTML, CSS, JavaScript',
 				url:"https://codepen.io/maiganpierce/full/veMmgw/",
-				background:"content/weather-app.PNG"
+				background:"content/placeholder1.png"
 			},
 			{
 				title:'Random Quote Machine',
 				description:'Uses JavaScript, AngularJS, HTML, CSS, Bootstrap',
 				url:"https://codepen.io/maiganpierce/full/GMebGO/",
-				background:"content/quote-machine.PNG"
+				background:"content/placeholder1.png"
 			}];
 
 			$scope.onClickProject = function(url){
